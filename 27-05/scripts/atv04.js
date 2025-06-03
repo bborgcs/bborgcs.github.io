8b6fd53c196cc37b6985af0e4baef72cde6557b1
bborgcs <babiborges2007@icloud.com>
2025-06-03 13:38:52 -0300

    falta 1 :(

--- 27-05/scripts/atv04.js
+++ 27-05/scripts/atv04.js
@@ -5,13 +5,27 @@ function ex04() {
     const input = form.querySelector('input[name="in_04"]').value
     alert(input)
     form.reset()
+
+    let lista = input.split(" ")
+    document.getElementById("output").textContent = `${resolve04(lista)}`
 }
 
-function resolve04() {
-
+function resolve04(numeros) {
+    let resultado = numeros.filter(function(n) {
+        return filter_perfeito(Number(n));
+    });
+    return resultado;
 }
 
 const filter_perfeito = (val) => {
+    let soma=0
+    for(i=0;i<val;i++){
+        let resul=val%i
+        if(resul==0){
+            soma+=i
+        }
+    }
 
+    return soma === val
 }
 
--- 27-05/scripts/atv05.js
+++ 27-05/scripts/atv05.js
@@ -5,12 +5,29 @@ function ex05() {
     const input = form.querySelector('input[name="in_05"]').value
     alert(input)
     form.reset()
+
+    document.getElementById("output").innerHTML = resolve05(input)
+
 }
 
-function resolve05(obj) {
+function resolve05(jsonString) {
+    try{
+        const dados = JSON.parse(jsonString)
+        const obj = new construtora(dados)
 
+        let resultado = '';
+        for (let elemento in obj) {
+            resultado += `${elemento}: ${obj[elemento]}<br>`;
+        }
+
+        return resultado;
+    } catch (e) {
+        return "Erro ao processar JSON: " + e.message;
+    }
 }
 
 function construtora(data) {
-
+    for (let elemento in data) {
+        this[elemento] = data[elemento]
+    }
 }
